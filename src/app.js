aacc57d769ebd91dd47bce9c9f66e2b73fca8895
Moises Amador Medina Gonzalez <amadargonzalez2012@gmail.com>
2026-07-18 23:50:22 -0600

    kj1

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(
     credentials: true,
 })
 );
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 app.use(morgan("dev"));
 
 app.use(async (_req, _res, next) => {
--- src/routers/auth.routers.js
+++ src/routers/auth.routers.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { register,login, profile, logoutAll} from "../controllers/auth.controller.js";
+import { register, login, profile, logoutAll, updateProfile } from "../controllers/auth.controller.js";
 import {auth} from "../middleware/auth.js";
 
 
@@ -8,6 +8,7 @@ const router = Router();
 router.post(`/register`, register);
 router.post(`/login`, login);
 router.get(`/profile`, auth, profile);
+router.put(`/profile`, auth, updateProfile);
 router.post(`/logout-all`, auth, logoutAll);
 
 export default router;
